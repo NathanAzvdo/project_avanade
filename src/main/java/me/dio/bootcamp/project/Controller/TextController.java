bbc67692a809b8cadb01627afba63d34753bea94
NathanAzvdo <nathan.azevedo28@gmail.com>
2025-03-12 23:37:11 -0300

    feat: response entity no controller

--- src/main/java/me/dio/bootcamp/project/Controller/TextController.java
+++ src/main/java/me/dio/bootcamp/project/Controller/TextController.java
@@ -5,6 +5,7 @@ import me.dio.bootcamp.project.Controller.Request.TextRequest;
 import me.dio.bootcamp.project.Controller.Response.TextResponse;
 import me.dio.bootcamp.project.entity.Text;
 import me.dio.bootcamp.project.service.TextService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,27 +23,31 @@ public class TextController {
     }
 
     @PostMapping("/save")
-    public TextResponse saveText(@RequestBody TextRequest textRequest, @RequestParam(defaultValue = "2") int lines) {
+    public ResponseEntity<TextResponse> saveText(@RequestBody TextRequest textRequest, @RequestParam(defaultValue = "2") int lines) {
         Text toText = TextMapper.toText(textRequest);
         Text savedText = textService.saveText(toText.getText(), lines);
-        return TextMapper.toTextResponse(savedText);
+        return ResponseEntity.ok().body(TextMapper.toTextResponse(savedText));
     }
 
     @GetMapping("/find/content")
-    public List<TextResponse> findTextByContent(@RequestBody TextRequest textRequest) {
+    public ResponseEntity<List<TextResponse>> findTextByContent(@RequestBody TextRequest textRequest) {
         Text toText = TextMapper.toText(textRequest);
         List<Text> texts = textService.findByContent(toText.getText());
-        return texts.stream()
+        return ResponseEntity.ok()
+                .body(texts.stream()
                 .map(TextMapper::toTextResponse)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList())
+                );
     }
 
     @GetMapping("/find")
-    public List<TextResponse> findAllTexts() {
+    public ResponseEntity<List<TextResponse>> findAllTexts() {
         List<Text> texts = textService.findAll();
-        return texts.stream()
+        return ResponseEntity.ok()
+                .body(texts.stream()
                 .map(TextMapper::toTextResponse)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList())
+                );
     }
 
     @GetMapping("/find/{id}")
@@ -56,10 +61,11 @@ public class TextController {
     }
 
     @PutMapping("/update/{id}")
-    public TextResponse updateText(@PathVariable Long id, @RequestBody TextRequest textRequest, @RequestParam(defaultValue = "2") int lines) {
+    public ResponseEntity<TextResponse> updateText(@PathVariable Long id, @RequestBody TextRequest textRequest, @RequestParam(defaultValue = "2") int lines) {
         Text toText = TextMapper.toText(textRequest);
         Text updatedText = textService.updateText(id, toText.getText(), lines);
-        return TextMapper.toTextResponse(updatedText);
+        return ResponseEntity.ok()
+                .body(TextMapper.toTextResponse(updatedText));
     }
 
     @DeleteMapping("/delete/{id}")
